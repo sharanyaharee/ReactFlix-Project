b3b0855d1938f20f15a89da8a0d99a6ca218d042
Sharanya <sharu.sesh@gmail.com>
2023-04-12 11:23:32 +0400

    second

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import MovieCard from "./MovieCard";
+import { useState,useEffect } from "react";
 import Wrapper from "./Wrapper";
-import { popularMovies } from "../utils/data";
+//import { popularMovies } from "../utils/data";
+import axios from "axios";
+import Pagination from "./Pagination";
+
 function Main() {
+  
+  const [popMovies, setPopMovies] =useState([]);
+  const [page,setPage]= useState(1);
+  const [totalPages,setTotalPages] = useState(null);
+
+useEffect(()=>{axios( {method:"get",
+url:  "https://api.themoviedb.org/3/movie/popular",
+params:{
+api_key : "e3ef60114f3455d412ea55db83f798b2",
+page:page
+},
+}).then(({data}) => {setPopMovies(data.results); 
+setTotalPages(data.total_pages)})},[page]);
+   console.log("jnwhgugWV",totalPages)
+  
+
+
   return (
     <main>
       <div className="movieSlider">
@@ -10,12 +31,15 @@ function Main() {
         <Wrapper>
           <h2 className="sectionTitle">Popular Movies </h2>
           <div className="gallery">
-            {popularMovies.map(movie => <MovieCard key={movie.id}  movie={movie} />)}
+           {popMovies.map(movie =>  <MovieCard key={movie.id}  movie={movie} />)}
           </div>
+          <Pagination page={page} setPage={setPage} totalPages={totalPages}  />
         </Wrapper>
       </div>
     </main>
   );
 }
+
+
 
 export default Main;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-function MovieCard({ movie: {title,rating,poster_path} }) {
+function MovieCard({ movie: {title,vote_average,poster_path} }) {
   return (
     <div className="movieCard">
-      <img className="moviePoster" src={poster_path} alt="" />
+      <img className="moviePoster" src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt="" />
       <div className="movieCardFooter">
         <div className="movieCardDetails">
           <h2 className="movieCardTitle">{title}</h2>
-          <p className="movieCardRating">{rating}</p>
+          <p className="movieCardRating">{vote_average}</p>
         </div>
         <div className="favButton">#</div>
       </div>
